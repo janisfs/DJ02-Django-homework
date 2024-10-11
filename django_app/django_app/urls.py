fde11d8af1a8884f3a6a9dd494ca222e8b3e03f6
janisfs <janisfs@mail.ru>
2024-10-11 19:01:15 +0300

    add new code

--- django_app/django_app/urls.py
+++ django_app/django_app/urls.py
@@ -16,7 +16,11 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from . import views
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
+    path('page1/', views.page1, name='page1'),
+    path('page2/', views.page2, name='page2'),
+    path('page3/', views.page3, name='page3'),
+    path('page4/', views.page4, name='page4'),
 ]
--- django_app/myapp/views.py
+++ django_app/myapp/views.py
@@ -1,3 +1,16 @@
 from django.shortcuts import render
 
 # Create your views here.
+from django.shortcuts import render
+
+def page1(request):
+    return render(request, 'myapp/page1.html')
+
+def page2(request):
+    return render(request, 'myapp/page2.html')
+
+def page3(request):
+    return render(request, 'myapp/page3.html')
+
+def page4(request):
+    return render(request, 'myapp/page4.html')
